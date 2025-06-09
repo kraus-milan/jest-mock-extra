3c2b423172a7d6b4e7326f3d09af4c669f260607
Milan Kraus <milan.kraus@sick.de>
2025-06-09 15:54:58 +0200

    remove coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+/** @returns {Promise<import('jest').Config>} */
+module.exports = async () => ({
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/lib/'],
     coveragePathIgnorePatterns: ['/node_modules/', '/lib/'],
-};
+    coverageReporters: ['json-summary', 'html'],
+});
